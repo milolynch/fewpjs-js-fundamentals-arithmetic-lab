8628ccb8331ec6514b59b9e666b927e6fe2756dd
Milo_Lynch <Milobravman20@gmail.com>
2020-10-18 20:22:45 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
+let num1 = 2
+let num2 = 31
+
+const multiply = function()
+{
+    return num1*num2;
+}
+
+console.log(multiply())
+
+const random = function() {
+    return Math.floor(Math.random()*10) + 1;
+}
+console.log(random())
+
+const mod = () => {return 24%5}
+
+console.log(mod())
+
+let max = () => {return Math.max(1, 2, 3, 4, 20);}
+
+console.log(max())
 // Write your code here
